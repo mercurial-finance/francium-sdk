c16a741e7a0407ffda9d73352e54cfc77b5a30c8
McSam <sam@raccoons.dev>
2022-11-22 18:41:20 +0800

    remove more unused deps

--- src/model/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./price/token";
-export * from "./price/orca";
-export * from "./price/raydium";
-export * from "./lend";
-export * from "./lend/reward";
-export * from "./lend/balance";
-export * from "./farm";
--- src/utils/trx/setUnit.ts
+++
@@ -1,5 +0,0 @@
-import { ComputeBudgetProgram } from '@solana/web3.js';
-
-export default ComputeBudgetProgram.setComputeUnitLimit({
-  units: 400000
-});
